0bf7cd9d18275da70904a212d33a9bcd10243bcc
ASJ91114 <75980627+ASJ91114@users.noreply.github.com>
2021-07-02 16:21:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+song1 = "";
+song2 = "";
+song1Status = "";
+song2Status = "";
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+rightWristX = 0;
+rightWristY = 0;
+leftWristX = 0;
+leftWristY = 0;
+
+function preload() {
+    song1 = loadSound("defaulter.mp3");
+    song2 = loadSound("dhaka.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600, 480);
+    canvas.position(370, 180);
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+function modelLoaded() {
+    console.log("PoseNet Is Initialized");
+}
+
+function draw() {
+    image(video, 0, 0, 600, 480);
+}
+
+function play() {
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+    }
+}
